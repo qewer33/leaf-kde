882b7ba95680d4d38198e17b446d6aba29654c6c
qewer33 <yerdemergul@gmail.com>
2022-03-09 23:36:16 +0300

    Refractor scripts and write package.rb

--- install.rb
+++ install.rb
@@ -1,20 +1,11 @@
 #!/usr/bin/env ruby
 
+require_relative 'components.rb' rescue LoadError
 require 'fileutils'
-
-Component = Struct.new(:name, :path, :install_path)
-
-COMPONENTS = [
-    Component.new("look-and-feel", "look-and-feel/.", "#{Dir.home}/.local/share/plasma/look-and-feel"),
-    Component.new("desktoptheme", "desktoptheme/.", "#{Dir.home}/.local/share/plasma/desktoptheme"),
-    Component.new("aurorae", "aurorae/.", "#{Dir.home}/.local/share/aurorae/themes"),
-    Component.new("color-schemes", "color-schemes/.", "#{Dir.home}/.local/share/color-schemes"),
-    Component.new("wallpapers", "wallpapers/.", "#{Dir.home}/.local/share/wallpapers"),
-]
 
 COMPONENTS.each do |comp|
     puts "Installing component: #{comp.name}..."
     puts "Copying contents of #{comp.path} to #{comp.install_path}..."
-    FileUtils.cp_r(comp.path, comp.install_path)
+    FileUtils.cp_r(comp.path+"/.", comp.install_path)
     puts "Component installed: #{comp.name}"
 end
--- package.rb
+++ package.rb
@@ -1,1 +1,14 @@
 #!/usr/bin/env ruby
+
+require_relative 'components.rb' rescue LoadError
+require 'fileutils'
+
+COMPONENTS.each do |comp|
+    puts "Packaging component: #{comp.name}..."
+    puts "Packaging contents of #{comp.path} to ./build/..."
+    Dir.entries(comp.path).select { |e| !e.include?(".") } .each do |entry|
+        system("tar", "-czvf", "#{comp.name} #{entry}.tar.gz", "-C", "#{comp.path}/#{entry}/", ".")
+        FileUtils.mv("#{comp.name} #{entry}.tar.gz", "build")
+    end
+    puts "Component packaged: #{comp.name}"
+end
