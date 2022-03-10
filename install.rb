c7b1dd857227a3641c268ed86113504fd8f8e850
qewer33 <yerdemergul@gmail.com>
2022-03-10 21:17:02 +0300

    Update scripts

--- install.rb
+++ install.rb
@@ -6,6 +6,7 @@ require 'fileutils'
 COMPONENTS.each do |comp|
     puts "Installing component: #{comp.name}..."
     puts "Copying contents of #{comp.path} to #{comp.install_path}..."
+    # copy the the component folder to the component install directory
     FileUtils.cp_r(comp.path+"/.", comp.install_path)
     puts "Component installed: #{comp.name}"
 end
